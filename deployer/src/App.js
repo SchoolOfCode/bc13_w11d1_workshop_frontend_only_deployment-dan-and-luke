4474e37fa94d470c55f7a387ebea690689386ae7
Cmndgrab <110107052+Cmndgrab@users.noreply.github.com>
2022-12-05 15:10:44 +0000

    jin

--- deployer/src/App.js
+++ deployer/src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          JIN KAZAMA WINS 
-        </a>
+        <h1>JIN KAZAMA</h1>
       </header>
     </div>
   );
